017a97864f7a5b3abcc9ec485d2e54c1585fb1cc
Roy Segall <roy.segall@gizra.com>
2015-05-14 11:23:52 +0300

    First work.

--- example/example.js
+++ example/example.js
@@ -1,8 +1,8 @@
 /**
- * @api {get} /user/:id Read data of a User
+ * @api {get} /basic/:id Endpoints
  * @apiVersion 0.3.0
- * @apiName GetUser
- * @apiGroup User
+ * @apiName GetEndpoints
+ * @apiGroup Basic
  * @apiPermission admin
  *
  * @apiDescription Compare Verison 0.3.0 with 0.2.0 and you will see the green markers with new items in version 0.3.0 and red markers with removed items since 0.2.0.
@@ -32,37 +32,148 @@
  *       "error": "NoAccessRight"
  *     }
  */
-function getUser() { return; }
+function getEndpoints() { return; }
 
 /**
- * @api {post} /user Create a new User
+ * @api {get} /basic/:id Authentication
  * @apiVersion 0.3.0
- * @apiName PostUser
- * @apiGroup User
- * @apiPermission none
+ * @apiName GetUser
+ * @apiGroup Basic
+ * @apiPermission admin
  *
- * @apiDescription In this case "apiErrorStructure" is defined and used.
- * Define blocks with params that will be used in several functions, so you dont have to rewrite them.
+ * @apiDescription Compare Verison 0.3.0 with 0.2.0 and you will see the green markers with new items in version 0.3.0 and red markers with removed items since 0.2.0.
  *
- * @apiParam {String} name Name of the User.
+ * @apiParam {Number} id The Users-ID.
  *
- * @apiSuccess {Number} id         The new Users-ID.
+ * @apiExample Example usage:
+ * curl -i http://localhost/user/4711
  *
- * @apiUse CreateUserError
+ * @apiSuccess {Number}   id            The Users-ID.
+ * @apiSuccess {Date}     registered    Registration Date.
+ * @apiSuccess {Date}     name          Fullname of the User.
+ * @apiSuccess {String[]} nicknames     List of Users nicknames (Array of Strings).
+ * @apiSuccess {Object}   profile       Profile data (example for an Object)
+ * @apiSuccess {Number}   profile.age   Users age.
+ * @apiSuccess {String}   profile.image Avatar-Image.
+ * @apiSuccess {Object[]} options       List of Users options (Array of Objects).
+ * @apiSuccess {String}   options.name  Option Name.
+ * @apiSuccess {String}   options.value Option Value.
+ *
+ * @apiError NoAccessRight Only authenticated Admins can access the data.
+ * @apiError UserNotFound   The <code>id</code> of the User was not found.
+ *
+ * @apiErrorExample Response (example):
+ *     HTTP/1.1 401 Not Authenticated
+ *     {
+ *       "error": "NoAccessRight"
+ *     }
  */
-function postUser() { return; }
+function getAuth() { return; }
 
 /**
- * @api {put} /user/:id Change a User
+ * @api {get} /basic/:id Content
  * @apiVersion 0.3.0
- * @apiName PutUser
- * @apiGroup User
- * @apiPermission none
+ * @apiName GetContent
+ * @apiGroup Content
+ * @apiPermission admin
  *
- * @apiDescription This function has same errors like POST /user, but errors not defined again, they were included with "apiErrorStructure"
+ * @apiDescription Compare Verison 0.3.0 with 0.2.0 and you will see the green markers with new items in version 0.3.0 and red markers with removed items since 0.2.0.
  *
- * @apiParam {String} name Name of the User.
+ * @apiParam {Number} id The Users-ID.
  *
- * @apiUse CreateUserError
+ * @apiExample Example usage:
+ * curl -i http://localhost/user/4711
+ *
+ * @apiSuccess {Number}   id            The Users-ID.
+ * @apiSuccess {Date}     registered    Registration Date.
+ * @apiSuccess {Date}     name          Fullname of the User.
+ * @apiSuccess {String[]} nicknames     List of Users nicknames (Array of Strings).
+ * @apiSuccess {Object}   profile       Profile data (example for an Object)
+ * @apiSuccess {Number}   profile.age   Users age.
+ * @apiSuccess {String}   profile.image Avatar-Image.
+ * @apiSuccess {Object[]} options       List of Users options (Array of Objects).
+ * @apiSuccess {String}   options.name  Option Name.
+ * @apiSuccess {String}   options.value Option Value.
+ *
+ * @apiError NoAccessRight Only authenticated Admins can access the data.
+ * @apiError UserNotFound   The <code>id</code> of the User was not found.
+ *
+ * @apiErrorExample Response (example):
+ *     HTTP/1.1 401 Not Authenticated
+ *     {
+ *       "error": "NoAccessRight"
+ *     }
  */
-function putUser() { return; }
+function getContent() { return; }
+
+/**
+ * @api {get} /basic/:id Files
+ * @apiVersion 0.3.0
+ * @apiName GetFiles
+ * @apiGroup Content
+ * @apiPermission admin
+ *
+ * @apiDescription Compare Verison 0.3.0 with 0.2.0 and you will see the green markers with new items in version 0.3.0 and red markers with removed items since 0.2.0.
+ *
+ * @apiParam {Number} id The Users-ID.
+ *
+ * @apiExample Example usage:
+ * curl -i http://localhost/user/4711
+ *
+ * @apiSuccess {Number}   id            The Users-ID.
+ * @apiSuccess {Date}     registered    Registration Date.
+ * @apiSuccess {Date}     name          Fullname of the User.
+ * @apiSuccess {String[]} nicknames     List of Users nicknames (Array of Strings).
+ * @apiSuccess {Object}   profile       Profile data (example for an Object)
+ * @apiSuccess {Number}   profile.age   Users age.
+ * @apiSuccess {String}   profile.image Avatar-Image.
+ * @apiSuccess {Object[]} options       List of Users options (Array of Objects).
+ * @apiSuccess {String}   options.name  Option Name.
+ * @apiSuccess {String}   options.value Option Value.
+ *
+ * @apiError NoAccessRight Only authenticated Admins can access the data.
+ * @apiError UserNotFound   The <code>id</code> of the User was not found.
+ *
+ * @apiErrorExample Response (example):
+ *     HTTP/1.1 401 Not Authenticated
+ *     {
+ *       "error": "NoAccessRight"
+ *     }
+ */
+function getFiles() { return; }
+
+/**
+ * @api {get} /basic/:id Terms and vocabularies
+ * @apiVersion 0.3.0
+ * @apiName GetTerms
+ * @apiGroup Content
+ * @apiPermission admin
+ *
+ * @apiDescription Compare Verison 0.3.0 with 0.2.0 and you will see the green markers with new items in version 0.3.0 and red markers with removed items since 0.2.0.
+ *
+ * @apiParam {Number} id The Users-ID.
+ *
+ * @apiExample Example usage:
+ * curl -i http://localhost/user/4711
+ *
+ * @apiSuccess {Number}   id            The Users-ID.
+ * @apiSuccess {Date}     registered    Registration Date.
+ * @apiSuccess {Date}     name          Fullname of the User.
+ * @apiSuccess {String[]} nicknames     List of Users nicknames (Array of Strings).
+ * @apiSuccess {Object}   profile       Profile data (example for an Object)
+ * @apiSuccess {Number}   profile.age   Users age.
+ * @apiSuccess {String}   profile.image Avatar-Image.
+ * @apiSuccess {Object[]} options       List of Users options (Array of Objects).
+ * @apiSuccess {String}   options.name  Option Name.
+ * @apiSuccess {String}   options.value Option Value.
+ *
+ * @apiError NoAccessRight Only authenticated Admins can access the data.
+ * @apiError UserNotFound   The <code>id</code> of the User was not found.
+ *
+ * @apiErrorExample Response (example):
+ *     HTTP/1.1 401 Not Authenticated
+ *     {
+ *       "error": "NoAccessRight"
+ *     }
+ */
+function getTerms() { return; }
